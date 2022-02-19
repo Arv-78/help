48a0bb3c0d33204f156da8075ae07d5fb46d8dd3
Arv-78 <valorant8228@gmail.com>
2022-02-19 17:01:04 +0530

    get unique elements from a range c++

--- a.cpp
+++ a.cpp
@@ -3,18 +3,23 @@
 */
 
 #include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-#define INF INFINITY; // macro
-// #define pi 3.142;
-#define REP(i, a, b) for (int i = a; i <= b; i++)
-#define SQ(a) (a) * (a)
 
-int main()
-{   
-	double pi = 2 * acos(0.0);
-	int n; scanf("%d",&n);
-	printf("%.*lf",n,pi);
-	
+using namespace std;
+
+#define ALL(x) 
+#define UNIQUE(c) 
+
+int main(){
+	//gives unique elements in the array
+	int a[] = {1, 2, 2, 2, 3, 3, 2, 2, 1};
+	vector<int> v(a, a + 9);
+	sort(v.begin(), v.end());
+	//unique replaces the first duplicate elemets with unique i.e. {1, 2, 3,...}
+	//unique returns iterator to last element which is not removed
+	int d = (unique(v.begin(), v.end()) - v.begin()); //gives distance between to iterators
+	v.resize(d);//resize to d = 3 elements
+	for(int i = 0; i < d; i++){
+		printf("%d\n", v[i]);
+	}
 	return 0;
 }
