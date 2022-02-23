7b32e3c426d7ae22cce0b754f7c6f79caecbf4d8
Arv-78 <valorant8228@gmail.com>
2022-02-23 17:04:36 +0530

    convert str of base X into another base string Y Java

--- java.java
+++ java.java
@@ -1,24 +1,10 @@
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.*;
+import java.math.BigInteger;
+
 public class java
 {
-    public static void main(String[] args) throws ParseException {
-     
-      // Calendar c1 = Calendar.getInstance();
-      // c1.set(Calendar.MONTH, 11);
-      // c1.set(Calendar.DATE, 05);
-      // c1.set(Calendar.YEAR, 1996);
-      // Date date = c1.getTime();
-      // String s = new SimpleDateFormat("EEEE").format(date);
-      // System.out.println(s);
-
-      String inputDate = "07/08/2001";
-      SimpleDateFormat format1 = new SimpleDateFormat("dd/MM/yyyy");
-      Date dt1 = format1.parse(inputDate);
-      DateFormat format2 = new SimpleDateFormat("EEEE"); 
-      String finalDay = format2.format(dt1);
-      System.out.println(finalDay);
+    public static void main(String[] args) {
+      String str = "FF"; int X = 16, Y = 10;
+     System.out.println(new BigInteger(str, X).toString(Y));
+      
     }
 } 
