9ad8354556c1a7424528782a4b2879ed74b9a876
Arv-78 <valorant8228@gmail.com>
2022-02-23 15:34:33 +0530

    find a value in sorted list to integers by binary_search() in c++

--- a.cpp
+++ a.cpp
@@ -11,19 +11,9 @@ typedef pair<ii, int> iii;
 
 
 int main(){
-	vector <iii> v;
+	int n = 5, L[] = {10, 7, 5, 20, 8}, v = 7;
+	sort(L, L + n);
 
-    iii A = make_pair(ii(5, 24), -1982);
-	iii B = make_pair(ii(5, 24), -1980);
-	iii C = make_pair(ii(11, 13), -1983);
-	v.push_back(A);
-	v.push_back(B);
-	v.push_back(C);
-
-	sort(v.begin(), v.end());
-
-	for(int i = 0; i<v.size(); i++){
-		printf("%d \t %d \t %d \n", v[i].first.first, v[i].first.second, v[i].second);
-	}
+	printf("%d\n", binary_search(L, L + n, v));
 	return 0;
 }
