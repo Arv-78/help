42e7ddd99a7f83d6ee07c0c1f109781ca4836de4
Arv-78 <valorant8228@gmail.com>
2022-02-24 14:20:08 +0530

    string.replaceAll(regex, replaceStr) in java

--- java.java
+++ java.java
@@ -3,8 +3,9 @@ import java.math.BigInteger;
 public class java
 {
     public static void main(String[] args) {
-      String str = "FF"; int X = 16, Y = 10;
-     System.out.println(new BigInteger(str, X).toString(Y));
+      String S = "line: a70 and z72 will be replaced,  aa24 and a872 will not";
+
+      System.out.println(S.replaceAll("(^| )+[a-z][0-9][0-9]($| )+", "***"));
       
     }
 } 
