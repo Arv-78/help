9b39a014b926be0074ccb909bddd7c8a1c31a3c8
Arv-78 <valorant8228@gmail.com>
2022-08-30 17:10:45 +0530

    01234 98765 all distinct numbers

--- a.cpp
+++ a.cpp
@@ -9,61 +9,18 @@ int main()
         freopen("input.txt", "r", stdin);
         freopen("output.txt", "w", stdout);
     #endif
-   //1.scan 2.check values for each combination 3.get greatest value
-   int a, b, c, d, e;
-   int val[6];
-   bool crds[52];
-   while(scanf("%d %d %d %d %d", &a, &b, &c, &d, &e), (a || b || c || d || e)){
-    //combinitions ab-de ba-de bc-de cb-de ac-de ca-de
-    memset(val, 0, sizeof(val));
-    memset(crds, false, sizeof(crds));
-    crds[a - 1] = crds[b - 1] = crds[c - 1] = crds[d - 1] = crds[e - 1] = true;
-    
-    if(a > d && b > e)val[0] = -1;
-    else if((a > d && b < e) || (a < d && b > e)) val[0] = c + 1;
-    else if(a < d && b < e)val[0] = 1;
-
-    if(b > d && a > e)val[1] = -1;
-    else if((b > d && a < e) || (b < d && a > e)) val[1] = c + 1;
-    else if(b < d && a < e)val[1] = 1;
-
-    if(b > d && c > e)val[2] = -1;
-    else if((b > d && c < e) || (b < d && c > e)) val[2] = a + 1;
-    else if(b < d && c < e)val[2] = 1;
-
-    if(c > d && b > e)val[3] = -1;
-    else if((c > d && b < e) || (c < d && b > e)) val[3] = a + 1;
-    else if(c < d && b < e)val[3] = 1;
-
-    if(a > d && c > e)val[4] = -1;
-    else if((a > d && c < e) || (a < d && c > e)) val[4] = b + 1;
-    else if(a < d && c < e)val[4] = 1;
-
-    if(c > d && a > e)val[5] = -1;
-    else if((c > d && a < e) || (c < d && a > e)) val[5] = b + 1;
-    else if(c < d && a < e)val[5] = 1;
-
-    sort(val, val+6);
-    if(val[0] == -1){
-        cout<<-1<<'\n';
-    }else{
-        if(!crds[val[5] - 1]){
-            cout<<val[5]<<'\n';
-        }else{
-            int t = val[5] - 1;
-            while(crds[t]){
-                t++;
+    //print all numbers from 01234 *(2 to 79) == 98765
+    for(int N = 2; N <= 79; N++){
+        for(int fghij = 1234; fghij <= 98765 / N; fghij ++){
+            int abcde = fghij * N;
+            int tmp, used = (fghij < 10000);
+            tmp = fghij; while(tmp) {used |= 1 << (tmp % 10); tmp /= 10;}
+            tmp = abcde; while(tmp) {used |= 1 << (tmp % 10); tmp /= 10;}
+            if(used == (1 << 10) - 1){//check for all numbers are used 
+                printf("%0.5d / %0.5d = %d\n", abcde, fghij, N);
             }
-            if(t + 1 <= 52)
-            cout<<t+1<<'\n';
-            else 
-            cout<<-1<<'\n';
         }
     }
-
-
-   }
-
     
     return 0;
 }
