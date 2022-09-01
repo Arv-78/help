c72087bd25f852a3e4d8429ff70cc934f5c3a081
Arv-78 <valorant8228@gmail.com>
2022-09-01 08:21:19 +0530

    UVa 11742 Social Constraints

--- a.cpp
+++ a.cpp
@@ -9,18 +9,49 @@ int main()
         freopen("input.txt", "r", stdin);
         freopen("output.txt", "w", stdout);
     #endif
-    //print all numbers from 01234 *(2 to 79) == 98765
-    for(int N = 2; N <= 79; N++){
-        for(int fghij = 1234; fghij <= 98765 / N; fghij ++){
-            int abcde = fghij * N;
-            int tmp, used = (fghij < 10000);
-            tmp = fghij; while(tmp) {used |= 1 << (tmp % 10); tmp /= 10;}
-            tmp = abcde; while(tmp) {used |= 1 << (tmp % 10); tmp /= 10;}
-            if(used == (1 << 10) - 1){//check for all numbers are used 
-                printf("%0.5d / %0.5d = %d\n", abcde, fghij, N);
-            }
+    int n, m;
+    while(cin>>n>>m, (n||m)){   
+        int p[n];
+        for(int i = 0; i < n; i++) p[i] = i;
+
+        vector <tuple<int, int, int>> v;
+        for(int i = 0; i < m; i++){
+            int a, b, c;
+            cin>>a>>b>>c;
+            v.push_back(make_tuple(a, b, c));
         }
-    }
+        int counter = 0;
+        do{
+            if(m){
+                bool pass = true;
+                for(int i = 0; i < m; i++){
+                    int a, b, c; // store it
+                    a = get<0>(v[i]);
+                    b = get<1>(v[i]);
+                    c = get<2>(v[i]);
+                    int pos_a, pos_b;
+                    for(int i = 0; i < n; i++){
+                        if(p[i] == a) pos_a = i;
+                        if(p[i] == b) pos_b = i;
+                    }
+                    int mx,mn;
+                    mx = max(pos_a, pos_b);
+                    mn = min(pos_a, pos_b);
+
+                    if(!((c > 0 && mx - mn <= c) || (c < 0 && mx - mn >= c*(-1))))
+                        pass = false;
     
+                }
+
+                if(pass)counter++;
+
+            }else{
+                counter++;
+            }
+            
+        }while(next_permutation(p, p + n));
+
+        std::cout<<counter<<'\n';
+    }
     return 0;
 }
--- b.cpp
+++ b.cpp
@@ -1,7 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
         
 int main()
@@ -10,61 +7,38 @@ int main()
         freopen("input.txt", "r", stdin);
         freopen("output.txt", "w", stdout);
     #endif
-    vector<int> her(3), his(2);
-    vector<bool> available;
     
-    int card;
-    
-    while (cin >> her[0] >> her[1] >> her[2] >> his[0] >> his[1], her[0])
-    {
-        available.clear();
-        available.resize(53, true);
-        
-        for (int i = 0; i < 3; ++i)
-        {
-            available[her[i]] = false;
-            
-            if (i < 2)
-                available[his[i]] = false;
+	int n,m;
+    int a,b,c;
+    while(scanf("%d %d",&n,&m),n||m){
+        int res = 0;
+        vector<int> ppl = {0,1,2,3,4,5,6,7};
+        vector<int> seat = {0,1,2,3,4,5,6,7};
+        vector<tuple<int,int,int>> constraints;
+        for(int i=0;i<m;i++){
+            cin >> a >> b >> c;
+            constraints.push_back(make_tuple(a,b,c));
         }
-        
-        sort(her.begin(), her.end());
-        sort(his.begin(), his.end());
-        
-        card = 53;
-        
-        // Both are higher
-        if (his[0] > her[2])
-        {
-            for (int i = 1; i <= 52 && i < card; ++i)
-                if (available[i])
-                    card = i;
-        }
-        
-        // One is higher than all
-        if (his[1] > her[2])
-        {
-            //printf("Here %d\n", her[2]);
-            for (int i = her[2] + 1; i <= 52 && i < card; ++i)
-            {
-                
-                if (available[i])
-                    card = i;
+        do{
+            for(int i=0;i<n;i++) seat[ppl[i]] = i;
+            bool satisfy = true;
+            // check each constraint
+            for(int i=0;i<constraints.size() && satisfy;i++){
+                auto& tup = constraints[i];
+                int p1 = get<0>(tup), p2 = get<1>(tup), req = get<2>(tup);
+                int dist = abs(seat[p1] - seat[p2]);
+                if(req < 0){
+                    // must be req length apart
+                    satisfy = dist >= abs(req);
+                } else {
+                    // must be within req
+                    satisfy = dist <= req;
+                }
             }
-        }
-        
-        // Both higher than two of her cards
-        if (his[0] > her[1])
-        {
-             for (int i = her[1] + 1; i <= 52 && i < card; ++i)
-                if (available[i])
-                    card = i;
-        }
-        
-        if (card == 53)
-            card = -1;
-           
-        cout << card << '\n';
+            if(satisfy) res++;
+        }while(next_permutation(ppl.begin(),ppl.begin()+n));
+        cout << res << endl;
     }
 
+    return 0;
 }
