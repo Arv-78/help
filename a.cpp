1203a4fac13cd13a939224c76953649ec3a46bee
Arv-78 <valorant8228@gmail.com>
2022-09-02 08:02:41 +0530

    another n-queen problem

--- a.cpp
+++ a.cpp
@@ -3,55 +3,39 @@
 */
 #include <bits/stdc++.h>
 using namespace std;
+
+char board[15][15];
+int ans, n;
+bitset <30> rw, rd, ld;
+void backtrack(int c){
+    if(c == n) {ans++;}
+
+    for(int r = 0; r < n; r++){
+        if(board[r][c] != '*' && !rw[r] && !rd[r + c] && !ld[r - c + n - 1]){
+            rw[r] = rd[r + c] = ld[r - c + n - 1] = true;
+            backtrack(c + 1);
+            rw[r] = rd[r + c] = ld[r - c + n - 1] = false;
+        }
+    }
+}
+
 int main()
 {
     #ifndef ONLINE_JUDGE
         freopen("input.txt", "r", stdin);
         freopen("output.txt", "w", stdout);
     #endif
-    int n, m;
-    while(cin>>n>>m, (n||m)){   
-        int p[n];
-        for(int i = 0; i < n; i++) p[i] = i;
 
-        vector <tuple<int, int, int>> v;
-        for(int i = 0; i < m; i++){
-            int a, b, c;
-            cin>>a>>b>>c;
-            v.push_back(make_tuple(a, b, c));
-        }
-        int counter = 0;
-        do{
-            if(m){
-                bool pass = true;
-                for(int i = 0; i < m; i++){
-                    int a, b, c; // store it
-                    a = get<0>(v[i]);
-                    b = get<1>(v[i]);
-                    c = get<2>(v[i]);
-                    int pos_a, pos_b;
-                    for(int i = 0; i < n; i++){
-                        if(p[i] == a) pos_a = i;
-                        if(p[i] == b) pos_b = i;
-                    }
-                    int mx,mn;
-                    mx = max(pos_a, pos_b);
-                    mn = min(pos_a, pos_b);
-
-                    if(!((c > 0 && mx - mn <= c) || (c < 0 && mx - mn >= c*(-1))))
-                        pass = false;
-    
-                }
-
-                if(pass)counter++;
-
-            }else{
-                counter++;
+    int tcn = 0;
+    while(scanf("%d", &n), n){
+        for(int r = 0; r < n; r++)
+            for(int c = 0; c < n; c++){
+                char ch; cin>>ch; board[r][c] = ch;
             }
-            
-        }while(next_permutation(p, p + n));
 
-        std::cout<<counter<<'\n';
+        ans = 0;
+        backtrack(0);
+        printf("Case %d: %d\n", ++tcn, ans);                
     }
     return 0;
 }
