486c4359b17e979f440c4fa86162c18816bcb636
Arv-78 <valorant8228@gmail.com>
2022-09-14 06:46:26 +0530

    UVa 11450 wedding shopping dp problem

--- a.cpp
+++ a.cpp
@@ -3,6 +3,23 @@
 */
 #include <bits/stdc++.h>
 using namespace std;
+
+// int M, C, price[25][25];
+// int memo[210][25];
+
+// int shop(int money, int g){
+//     if(money < 0)return -10000000;
+//     if(g == C) return M - money;
+//     int &ans = memo[money][g];
+//     if(ans != -1)return ans;
+
+//     for(int model = 1; model <= price[g][0]; model++){
+//         ans = max(ans, shop(money - price[g][model], g + 1));
+//     }
+
+//     return ans;
+// }
+
 int main()
 {
     #ifndef ONLINE_JUDGE
@@ -10,37 +27,45 @@ int main()
         freopen("output.txt", "w", stdout);
     #endif
     
-    int n, l, w;
-    while(scanf("%d %d %d", &n, &l, &w) == 3){
-        vector <pair<double, double>> v(10000);
+    int price[25][25];
+    bool reachable[25][210];
 
-        for(int i = 0; i < n; i++){
-            double pos, r;
-            scanf("%lf %lf", &pos, &r);
-            double range = sqrt(r*r - w/2.0 * w/2.0);
-            v[i] = make_pair((double)pos - range, double(pos) + range);
-        }
-
-        sort(v.begin(), v.end());
-
-        
-
-
-        double last = 0.0, far = 0.0;
-        int ct = 0;
-        for(int i = 0; i <= v.size(); i++){
-            if(l == 0){ct = -1; break;}
-            if(far >= l){ct++; break;}
-            if(v[i].first <= last)far = max(far, v[i].second);
-            if(v[i].first > last){
-                last = far;
-                if(v[i].first > last){ct = -1; break;}
-                ct++;
-                i--;
+    int TC, score, M, C;
+    scanf("%d", &TC);
+    while(TC --){
+        scanf("%d %d", &M, &C);
+        for(int i = 0; i < C; i++){
+            scanf("%d", &price[i][0]);
+            for(int j = 1; j <= price[i][0]; j++){
+                scanf("%d", &price[i][j]);
             }
         }
-        if(far < l)cout<<"-1\n"; 
-        else cout<<ct<<'\n';
+
+        memset(reachable, false, sizeof reachable);
+        //filling base cases
+        for(int i = 1; i <= price[0][0]; i++){
+            if(M - price[0][i] >= 0) reachable[0][M - price[0][i]] = true;
+        }
+
+        //filling dp table bottom up approch
+
+        for(int g = 1; g < C; g++){
+            for(int money = 0; money < M; money++){
+                if(reachable[g - 1][money]){
+                    for(int k = 1; k <= price[g][0]; k++){
+                        if(money - price[g][k] >= 0){
+                            reachable[g][money - price[g][k]] = true;
+                        }
+                    }
+                }
+            }
+        }
+        int money;
+        for(money = 0; money < M && !reachable[C - 1][money]; money++);
+
+        if(money == M) printf("no solution\n");
+        else printf("%d\n", M - money);
+        
     }
 
 
