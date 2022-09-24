56ae3348e237176a04606d441fdb70757df94903
Arv-78 <valorant8228@gmail.com>
2022-09-24 10:37:38 +0530

    coin change var - 2 edit

--- a.cpp
+++ a.cpp
@@ -6,17 +6,19 @@ using namespace std;
 
 
 int V = 10, n = 2, coinValue[] = {1, 5};
-
+int memo[10][1000]; //memo[type][value];
 int ways(int type, int value){   //O(nV)
     
     if(value == 0) return 1;
     if(value < 0 || type == n) return 0;
+
+    if(memo[type][value] != -1) return memo[type][value];
     //take or ignore
     int ans = 0;
-    for(int i = 0; i < n; i++){
-        ans = ways(type + 1, value) + ways(type, value - coinValue[i]);
-    }
-    return ans;
+    //for(int i = 0; i < n; i++){
+        ans = ways(type + 1, value) + ways(type, value - coinValue[type]);
+    //}
+    return memo[type][value] = ans;
 }
 
 
@@ -27,7 +29,7 @@ int main()
         freopen("output.txt", "w", stdout);
     #endif
     //coin change
-
+    memset(memo, -1, sizeof memo);
     cout<<ways(0, V);
 
 
