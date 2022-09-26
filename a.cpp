c2843007307f189b0874ed5c61e252c14312dcc5
Arv-78 <valorant8228@gmail.com>
2022-09-26 15:37:39 +0530

    UVa Garbage Heap 2d 1d range sum

--- a.cpp
+++ a.cpp
@@ -9,21 +9,61 @@ int main()
         freopen("input.txt", "r", stdin);
         freopen("output.txt", "w", stdout);
     #endif
-    int n;
-    while(scanf("%d", &n), n){
-        int N[10000];
-        for(int i = 0; i < n; i++)scanf("%d", &N[i]);
-        int max_streak = -999999, sum = 0;
-        for(int i = 0; i < n; i++){
-            sum += N[i];
-            if(sum > max_streak)
-                max_streak = sum;
-            if(sum < 0) sum = 0;
+
+    int N;
+    scanf("%d", &N);
+    int line = 0;
+    while(N--){
+        if(line) printf("\n");
+        line++;
+        int a, b, c;
+        scanf("%d", &a);
+        scanf("%d %d", &b, &c);
+
+        long long int garbage[30][30][30];
+
+        for(int i = 0; i < a; i ++) for(int j = 0; j < b; j++) for(int k = 0; k < c; k++)
+            cin>>garbage[i][j][k];
+
+        long long int opt = (1 << 31); //negative number
+        long long int tmp[30][30];
+        //third dimension
+        for(int i = 0; i < a; i++){//0 
+            //for 1 slice, again 1 + 2 slice, again 2nd individual slice
+            //reset tmp
+            memset(tmp, 0, sizeof tmp);
+            for(int j = i; j < a; j++){ //0-> 0 1
+                //store values in tmp
+                for(int k = 0; k < b; k++)
+                    for(int l = 0; l < c; l++)
+                        tmp[k][l] += garbage[j][k][l];
+                
+                //for inner slices
+                //2d range sum
+                long long int inner_slice[30]; long long int kadane[30];
+               
+                for(int k = 0; k < b; k++){//0 1
+                memset(inner_slice, 0, sizeof inner_slice);
+                    for(int l = k; l < b; l++){//0 -> 0 1
+
+                        for(int m = 0; m < c; m++){
+                            inner_slice[m] += tmp[l][m];
+                        }
+
+                        //1d sum //check
+                        long long int sum = 0;
+                        for(int d = 0; d < c; d++){
+                            sum += inner_slice[d];
+                            if(sum > opt) opt = sum;
+                            if(sum < 0) sum = 0;
+                        }
+
+                    }
+                }
+            }
         }
-        if(max_streak <= 0)
-            printf("Losing streak.\n");
-        else
-            printf("The maximum winning streak is %d.\n", max_streak);
+        cout<<opt<<'\n';
+
     }
     
     return 0;
