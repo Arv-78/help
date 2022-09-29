ceb9e78011bb63ce0892b6579f06e9761f8dc1f7
Arv-78 <valorant8228@gmail.com>
2022-09-29 16:50:17 +0530

    another ans for 10074 - Take the Land

--- a.cpp
+++ a.cpp
@@ -19,27 +19,25 @@ int main()
             }
         
         int lagrest_land = 0;
-        for(int i = 0; i < M; i++)
-            for(int j = 0; j < N; j++)
-                for(int k = i; k < M; k++){
-                    int flag = 0;
-                    for(int l = j; l < N; l++){
-                        int sum = 0; 
-                        if(flag)break;
-                        for(int a = i; a <= k; a++){
-                            if(flag) break;
-                            for(int b = j; b <= l; b++){
-                                if(land[a][b] == 1) {
-                                    sum = 0;
-                                    flag = 1;
-                                    break;
-                                }
-                                else sum++;
-                            }
-                        }
-                        lagrest_land = max(sum, lagrest_land);
+        int tmp, length, width;
+        for(int i = 0; i < M; i++){
+            int sum[100];
+            memset(sum, 0, sizeof sum);
+            for(int j = i; j < M; j++){
+                for(int k = 0; k < N; k++){
+                    sum[k] += !land[j][k];
+                    if(k == 0 || tmp != length * width){
+                        length = 0; tmp = 0;
+                    }
+                    length++; width = j - i + 1;
+                    tmp += sum[k];
+
+                    if(tmp == length * width){
+                        lagrest_land = max(lagrest_land, tmp);
                     }
                 }
+            }
+        }
         
         printf("%d\n", lagrest_land);
             
