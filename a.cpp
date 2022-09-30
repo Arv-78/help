d1288fe292c4636da9000a6b09bf0ad3516c0ff1
Arv-78 <valorant8228@gmail.com>
2022-09-30 06:34:36 +0530

    Uva 10667 Largest Block

--- a.cpp
+++ a.cpp
@@ -10,41 +10,52 @@ int main()
         freopen("output.txt", "w", stdout);
     #endif
     
-    int M, N;
-    int land[105][105];
-    while(scanf("%d %d", &M, &N) == 2, M != 0, N != 0){
-        for(int i = 0; i < M; i++)
-            for(int j = 0; j < N; j++){
-                scanf("%d", &land[i][j]);
-            }
+    int tc, n, bks;
+    int board[105][105];
+    scanf("%d", &tc);
+
+    while(tc--){
+        memset(board, 0, sizeof board);//not working for 1
+        scanf("%d", &n); scanf("%d", &bks);
+        // scanf("%d %d", &n, &bks);
+        // cout<<n<<' '<<bks<<'\n';
         
-        int lagrest_land = 0;
-        int tmp, length, width;
-        for(int i = 0; i < M; i++){
+
+
+        int r1, c1, r2, c2;
+        while(bks --){
+            scanf("%d %d %d %d", &r1, &c1, &r2, &c2);
+            for(int i = r1 - 1; i < r2; i++)
+                for(int j = c1 - 1; j < c2; j++)
+                    board[i][j] = 1;
+        }
+
+        // for(int i = 0; i < n; i++){ for(int j = 0; j < n; j++)
+        // cout<<board[i][j]<<' '; cout<<endl;}
+        
+
+        int large_area = 0; int tmp, length, width;
+        for(int i = 0; i < n; i++){
             int sum[100];
             memset(sum, 0, sizeof sum);
-            for(int j = i; j < M; j++){
-                for(int k = 0; k < N; k++){
-                    sum[k] += !land[j][k];
+            for(int j = i; j < n; j++){
+                for(int k = 0; k < n; k++){
+                    sum[k] += !board[j][k];
                     if(k == 0 || tmp != length * width){
-                        length = 0; tmp = 0;
+                        tmp = 0; length = 0;
                     }
-                    length++; width = j - i + 1;
+                    length++, width = j - i + 1;
                     tmp += sum[k];
-
                     if(tmp == length * width){
-                        lagrest_land = max(lagrest_land, tmp);
+                        large_area = max(large_area, tmp);
                     }
                 }
             }
         }
-        
-        printf("%d\n", lagrest_land);
-            
-    }
-    
 
+        printf("%d\n", large_area);
 
+    } 
 
     return 0;
 }
