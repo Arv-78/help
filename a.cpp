22975e3dec6b97d4dd9ce8a3e3dbcb12d1a154e5
arv-8 <gandharv.patil@coriolis.co.in>
2024-11-15 11:39:46 +0530

    10388 - Snap

--- a.cpp
+++ a.cpp
@@ -1,23 +1,111 @@
+/*
+    author: arv
+*/
 #include <bits/stdc++.h>
 using namespace std;
-
-typedef tuple<int, int, int> iii;
-
-int main(){
-
+int main()
+{
     #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
     #endif
 
-    vector<iii> birthdays;
-    birthdays.emplace_back(5, 24, -1980);
-    birthdays.emplace_back(5, 24, -1982);
-    birthdays.emplace_back(11, 13, -1983); //82 80 83
+    int cs, turns, len_jane_cards, len_john_cards, j, k;
 
-    sort(birthdays.begin(), birthdays.end());
+    char jane_cards[105]; 
+    char john_cards[105]; 
+    char jane_fu_cards[105]; 
+    char john_fu_cards[105]; 
 
-    for(auto &[mm, dd, yyyy] : birthdays){
-        printf("%d %d %d\n", dd, mm, -yyyy);
+    scanf("%d", &cs);
+
+    while(cs--) {
+        scanf("%s", jane_cards);
+        scanf("%s", john_cards);
+
+        memset(jane_fu_cards, '\0', sizeof(jane_fu_cards));
+        memset(john_fu_cards, '\0', sizeof(john_fu_cards));
+
+        turns = 1001;
+        len_jane_cards = strlen(jane_cards);
+        len_john_cards = strlen(john_cards);
+
+        j = 0, k = 0;
+        while(--turns){
+            //one turn
+            jane_fu_cards[strlen(jane_fu_cards)] = jane_cards[j];
+            john_fu_cards[strlen(john_fu_cards)] = john_cards[k];
+
+            //if cards are same
+            if(jane_cards[j] == john_cards[k]){
+                int snap = rand() / 141 % 2;
+
+                if(snap == 0){ //Jane calls first
+                    strcat(jane_fu_cards, john_fu_cards);
+                    memset(john_fu_cards, '\0', sizeof(john_fu_cards));
+
+                    printf("Snap! for Jane: ");
+
+                    for(int l = strlen(jane_fu_cards) - 1; l >= 0; l--)
+                        printf("%c", jane_fu_cards[l]);
+
+                    printf("\n");
+
+                    //check for win
+                    //if dec is empty and face up pile is empty
+                    if(k == (len_john_cards - 1) && strlen(john_fu_cards) == 0){
+                        printf("%s\n", "Jane wins.");
+                        break;
+                    }
+
+                } else { //John calls first
+                    strcat(john_fu_cards, jane_fu_cards);
+                    memset(jane_fu_cards, '\0', sizeof(jane_fu_cards));
+
+
+                    printf("Snap! for John: ");
+
+                    for(int l = strlen(john_fu_cards) - 1; l >= 0; l--)
+                        printf("%c", john_fu_cards[l]);
+
+                    printf("\n");
+
+                    //check for win
+                    //if dec is empty and face up pile is empty
+                    if(j == (len_jane_cards - 1) && strlen(jane_fu_cards) == 0){
+                        printf("%s\n", "John wins.");
+                        break;
+                    }
+                }
+
+            }
+
+            //if dec is empty and face up pile is not empty
+            if(j == (len_jane_cards - 1) && strlen(jane_fu_cards) > 0) {
+                strcpy(jane_cards, jane_fu_cards);
+                len_jane_cards = strlen(jane_cards);
+                memset(jane_fu_cards, '\0', sizeof(jane_fu_cards));
+                j = -1;
+            }
+
+            if(k == (len_john_cards - 1) && strlen(john_fu_cards) > 0){
+                strcpy(john_cards, john_fu_cards);
+                len_john_cards = strlen(john_cards);
+                memset(john_fu_cards, '\0', sizeof(john_fu_cards));
+                k = -1;
+            }
+
+            j++; k++;
+        }
+
+
+        if(turns == 0){
+            printf("%s\n", "Keeps going and going ...");
+        }
+
+        if(cs) printf("\n");
     }
+
+
+    return 0;
 }
