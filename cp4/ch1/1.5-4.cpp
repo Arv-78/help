d4930cff729d9add0a69b7790b970477affedd58
arv-8 <gandharv.patil@coriolis.co.in>
2025-01-11 19:15:54 +0530

    1.5-4.cpp

---
+++ cp4/ch1/1.5-4.cpp
@@ -0,0 +1,47 @@
+/*
+    author: arv
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isVowel(char &c){
+    c = tolower(c);
+    if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
+        return true;
+    }
+    return false;
+}
+
+
+bool isDigit(char &c){
+    if(c <= '9' && c >= '0') return true;
+    return false;
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    char line[256];
+    fgets(line, 255, stdin);
+
+    // check for vowel, consonant and digit
+
+    int vowelc, consonantc, digitc;
+    vowelc = consonantc = digitc = 0;
+
+    for (int i = 0; i < strlen(line); i++){
+        if(isVowel(line[i])) vowelc++;
+        else if(isDigit(line[i])) digitc++;
+        else consonantc++;
+
+        printf("%c", tolower(line[i]));
+    }
+
+    printf("\n vowels: %d\n consonants: %d\n digits: %d\n", vowelc, consonantc, digitc);
+
+    return 0;
+}
